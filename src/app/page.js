7e0db6aa64ea0d01fc873eea5560adb984b61fcb
Naman <namans0297@gmail.com>
2025-08-10 02:07:12 +0530

    Added animations

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import WhatIDo from "../components/WhatIDo"
 import Story from "../components/Story"
 import ExperienceTimeline from "../components/ExperienceTimeline"
 import MatrixBackground from "../components/bg/MatrixBackground"
-import CursorTrail from "../components/bg/CursorTrail"
+// import CursorTrail from "../components/bg/CursorTrail"
 
 export default function Home() {
   const [bg, setBg] = useState('matrix')
@@ -18,7 +18,7 @@ export default function Home() {
       className="relative min-h-screen overflow-x-hidden bg-gradient-to-b from-neutral-950 via-neutral-950 to-neutral-900 text-white"
     >
       {bg === 'matrix' ? <MatrixBackground /> : null}
-      <CursorTrail
+      {/* <CursorTrail
         color="255,255,255"
         headWidth={18}
         tailMin={36}
@@ -26,7 +26,7 @@ export default function Home() {
         maxStep={1.2}
         decayPerSec={1200}
         zIndex={9}
-      />
+      /> */}
 
       <div className="relative z-10">
         <NavBar onChangeBg={setBg} currentBg={bg} />
